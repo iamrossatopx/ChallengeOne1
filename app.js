a29fbd251a13dbc34890ffbbf54ee1bde51676b5
I am [Px] <12368182+iamrossatopx@users.noreply.github.com>
2024-02-02 11:14:52 -0400

    tratamento de input aplicado

--- app.js
+++ app.js
@@ -1,15 +1,28 @@
+
+/*ajuste do conteudo a tela de acordo com o modo (portrait or landscape)*/
 if (window.visualViewport.width < window.visualViewport.height){
     document.getElementById('mainforjs').style['flex-direction'] = 'column';    
     document.getElementById('painel2forjs').style['width'] = '95vw';
-    console.log('portrait');
 }
 
 
 
 function criptografar() {
+
 }
 
+function descriptografar(){
 
-var botao = document.getElementById('#btn1');
-document.addEventListener('click', botao, criptografar());
+}
+
+function copiar(){
+    
+}
+
+let botao1 = document.getElementById('#btn1');
+let botao2 = document.getElementById('#btn2');
+let botao3 = document.getElementById('#btn3');
+document.addEventListener('click', botao1, criptografar());
+document.addEventListener('click', botao2, descriptografar());
+document.addEventListener('click', botao3, copiar());
 
