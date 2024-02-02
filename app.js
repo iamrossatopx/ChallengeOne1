0e9c6ef5e554537b5accd144c14e08feb09f2c44
I am [Px] <12368182+iamrossatopx@users.noreply.github.com>
2024-02-02 10:48:28 -0400

    fix ajust do layout em portrait mode

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
+if (window.visualViewport.width < window.visualViewport.height){
+    document.getElementById('mainforjs').style['flex-direction'] = 'column';    
+    document.getElementById('painel2forjs').style['width'] = '95vw';
+    console.log('portrait');
+}
+
+
+
 function criptografar() {
 }
 
